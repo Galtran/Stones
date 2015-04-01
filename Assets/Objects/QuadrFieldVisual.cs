3b0f8e0ab648b0ad778a1b3471f9da10e86e25e6
Galtran <ioj_and_mozg@mail.ru>
2015-04-01 12:06:19 +0300

    в добавок к предыдущему

---
+++ Assets/Objects/QuadrFieldVisual.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WarlikeStonesCore.Objects.Fileds;
+using UnityEngine;
+
+namespace Assets.Objects
+{
+	public class QuadrFieldVisual : QuadrField
+	{
+
+      GameObject[,] objectStones;
+
+      public QuadrFieldVisual(int size_x, int size_y) :
+         base(size_x, size_y)
+      {
+         objectStones = new GameObject[size_x, size_y];
+      }
+
+      public void SetObjectStone(int x, int y, GameObject obj)
+      {
+         objectStones[x, y] = obj;
+      }
+
+	}
+}
